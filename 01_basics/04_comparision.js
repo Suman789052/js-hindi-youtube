b0fe3a5e7b221f887e00636eac0082cd3720ee8a
Suman Chowdhury <124029545+Suman789052@users.noreply.github.com>
2025-06-17 04:24:51 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// not to do this type of comparesion these leads to not cleen code
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2);
+
+
+
