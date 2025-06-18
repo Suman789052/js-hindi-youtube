bbe53d1c8773b55e9b5d527d59b8f88f3d778e80
Suman Chowdhury <124029545+Suman789052@users.noreply.github.com>
2025-06-18 04:09:20 +0000

    datatypes-summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -10,8 +10,27 @@ const outsideTemp = null
 let userEmail;
 
 const id = Symbol('123')
-const anotherId
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Suman",
+    age: 24,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof id);
