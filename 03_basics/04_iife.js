98889f2395ed35c6c7a98b044c45ca9ecf682d72
Suman Chowdhury <124029545+Suman789052@users.noreply.github.com>
2025-06-27 04:51:26 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,13 @@
+// Immediatly Invoked Function Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);   
+}) ();
+
+( (name) => {
+    //simple IIFE
+    console.log(`DB CONNECTED TWO ${name}`);    
+} )('hitesh')
+
